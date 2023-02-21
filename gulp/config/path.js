069a3ce4cb4fe88a4dfd88a2af1a5398ef06e0d3
Ayrat <ayrat.klim1@yandex.ru>
2023-02-21 13:59:37 +0300

    fixed gulp working with images

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,39 +1,38 @@
 //Получаем имя папки проекта
-import * as nodePath from 'path';
-const rootFolder = nodePath.basename(nodePath.resolve());
+import * as nodePath from 'path'
+const rootFolder = nodePath.basename(nodePath.resolve())
 
-
-const buildFolder = './dist';
-const srcFolder = './src';
+const buildFolder = './dist'
+const srcFolder = './src'
 
 export const path = {
-    build: {
-        js: `${buildFolder}/js/`,
-        images: `${buildFolder}/img/`,
-        fonts: `${buildFolder}/fonts/`,
-        css: `${buildFolder}/css/`,
-        html: `${buildFolder}/`,
-        files: `${buildFolder}/files/`,
-    },
-    src: {
-        js: `${srcFolder}/js/app.js`,
-        images: `${srcFolder}/img/**/*.{jpg, jpeg, png, gif, webp}`,
-        svg: `${srcFolder}/img/**/*.svg`,
-        scss: `${srcFolder}/scss/style.scss`,
-        html: `${srcFolder}/*.html`, // pug
-        files: `${srcFolder}/files/**/*.*`,
-        svgicons: `${srcFolder}/svgicons/*.svg`
-    },
-    watch: {
-        js: `${srcFolder}/js/**/*.js`,
-        images: `${srcFolder}/img/**/*.{jpg, jpeg, png, svg, gif, webp}`,
-        scss: `${srcFolder}/scss/**/*.scss`,
-        html: `${srcFolder}/**/*.html`, // pug
-        files: `${srcFolder}/files/**/*.*`,
-    },
-    clean: buildFolder,
-    buildFolder: buildFolder,
-    srcFolder: srcFolder,
-    rootFolder: rootFolder,
-    ftp: `` // Папка на удаленном сервере
+  build: {
+    js: `${buildFolder}/js/`,
+    images: `${buildFolder}/img/`,
+    fonts: `${buildFolder}/fonts/`,
+    css: `${buildFolder}/css/`,
+    html: `${buildFolder}/`,
+    files: `${buildFolder}/files/`,
+  },
+  src: {
+    js: `${srcFolder}/js/app.js`,
+    images: `${srcFolder}/img/**/*.+(jpg|jpeg|png|gif|webp)`,
+    svg: `${srcFolder}/img/**/*.svg`,
+    scss: `${srcFolder}/scss/style.scss`,
+    html: `${srcFolder}/*.html`, // pug
+    files: `${srcFolder}/files/**/*.*`,
+    svgicons: `${srcFolder}/svgicons/*.svg`,
+  },
+  watch: {
+    js: `${srcFolder}/js/**/*.js`,
+    images: `${srcFolder}/img/**/*.{jpg, jpeg, png, svg, gif, webp}`,
+    scss: `${srcFolder}/scss/**/*.scss`,
+    html: `${srcFolder}/**/*.html`, // pug
+    files: `${srcFolder}/files/**/*.*`,
+  },
+  clean: buildFolder,
+  buildFolder: buildFolder,
+  srcFolder: srcFolder,
+  rootFolder: rootFolder,
+  ftp: ``, // Папка на удаленном сервере
 }
